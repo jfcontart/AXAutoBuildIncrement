0e84aacba32b6614c918c134c19605cda8a07f3f
Jean-François CONTART <kortex@MacBook-Pro-de-Jean-Francois.local>
2016-01-01 19:05:51 +0100

    update travis script

--- travis/script.sh
+++ travis/script.sh
@@ -1,1 +1,1 @@
-xcodebuild -project AXAutoBuildIncrement.xcodeproj -scheme AXAutoBuildIncrement -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
+xcodebuild -project AXAutoBuildIncrement.xcodeproj -scheme AXAutoBuildIncrement -configuration Debug -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES GCC_GENERATE_TEST_COVERAGE_FILES=YES
